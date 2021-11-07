402574e4d047275e07365781babaa3c68f4fc57a
zainurroziqin <75110898+zainurroziqin@users.noreply.github.com>
2021-11-08 00:23:59 +0700

    BKPM minggu ke-9

---
+++ app/src/main/java/com/example/belajarpreferences/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.belajarpreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView nama = findViewById(R.id.tv_namaMain);
+
+        nama.setText(Preferences.getLoggedInUser(getBaseContext()));
+
+        findViewById(R.id.button_logoutMain).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Preferences.clearLoggedInUser(getBaseContext());
+                startActivity(new Intent(getBaseContext(),LoginActivity.class));
+                finish();
+            }
+        });
+    }
+}
